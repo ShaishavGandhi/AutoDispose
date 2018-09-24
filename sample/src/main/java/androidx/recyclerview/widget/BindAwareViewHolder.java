384b7d1d5a9c8cab81ca9ade7b9dcd1f98b75901
shaishavgandhi05 <shaishgandhi@gmail.com>
2018-09-23 22:15:01 -0700

    Move to AndroidX Artifacts
    Signed-off-by: shaishavgandhi05 <shaishgandhi@gmail.com>

--- sample/src/main/java/androidx/recyclerview/widget/BindAwareViewHolder.java
+++ sample/src/main/java/androidx/recyclerview/widget/BindAwareViewHolder.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package android.support.v7.widget;
+package androidx.recyclerview.widget;
 
 import android.view.View;
 
@@ -38,25 +38,29 @@ public abstract class BindAwareViewHolder extends RecyclerView.ViewHolder {
 
   }
 
-  @Override void setFlags(int flags, int mask) {
+  @Override
+  void setFlags(int flags, int mask) {
     boolean wasBound = isBound();
     super.setFlags(flags, mask);
     notifyBinding(wasBound, isBound());
   }
 
-  @Override void addFlags(int flags) {
+  @Override
+  void addFlags(int flags) {
     boolean wasBound = isBound();
     super.addFlags(flags);
     notifyBinding(wasBound, isBound());
   }
 
-  @Override void clearPayload() {
+  @Override
+  void clearPayload() {
     boolean wasBound = isBound();
     super.clearPayload();
     notifyBinding(wasBound, isBound());
   }
 
-  @Override void resetInternal() {
+  @Override
+  void resetInternal() {
     boolean wasBound = isBound();
     super.resetInternal();
     notifyBinding(wasBound, isBound());
--- sample/src/main/kotlin/com/uber/autodispose/sample/ArchComponentActivity.kt
+++ sample/src/main/kotlin/com/uber/autodispose/sample/ArchComponentActivity.kt
@@ -16,9 +16,9 @@
 
 package com.uber.autodispose.sample
 
-import android.arch.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
